624299c769871565ef352780c8d1dcdb1ec8c3d4
harshil114 <harshilvispute114@gmail.com>
2023-05-22 17:00:17 +0530

    quiz is ready

--- istriangle.js
+++ istriangle.js
@@ -1,7 +1,6 @@
 const inputs = document.querySelectorAll('.angle-input');
 const isTriangleBtn = document.querySelector("#is-triangle-btn");
 const outputE1 = document.querySelector("#output");
-
 function calculateSumOfAngles(angle1,angle2,angle3){
     const sumOfAngles = angle1+angle2+angle3;
 
@@ -11,10 +10,10 @@ function calculateSumOfAngles(angle1,angle2,angle3){
 function isTriangle(){
     const sumOfAngles = calculateSumOfAngles(Number(inputs[0].value),Number(inputs[1].value),Number(inputs[2].value) );
     if(sumOfAngles === 180){
-        outputE1.innerText = "YES! It's Triangle";
+        outputE1.innerText = "YES! It's Triangle...Now you can go to quiz";
     }
     else{
-        outputE1.innerText = "NO! It's not Triangle";
+        outputE1.innerText = "NO! It's not Triangle....You can not go to the quiz";
     }
 }
 
